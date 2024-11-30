8f8ce08b2563ae1f3ef36b92280251ce807dfe49
louai60 <louaysaafi60@gmail.com>
2024-11-30 21:47:42 +0000

    installing tailwindcss

--- frontend/src/@types/clsx.d.ts
+++
@@ -1,5 +0,0 @@
-// frontend/src/@types/clsx.d.ts
-declare module 'clsx' {
-    export type ClassValue = string | number | ClassValue[] | { [key: string]: any };
-    export default function clsx(...inputs: ClassValue[]): string;
-  }
